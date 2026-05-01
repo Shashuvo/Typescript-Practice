6813e2553aaf57548a44ba995623034c82f276be
Shashuvo <shahariat2810@gmail.com>
2026-05-01 21:01:55 +0600

    array & object destructuring for TS practiced

---
+++ src/destructuring.ts
@@ -0,0 +1,26 @@
+// destructuring
+
+// object destructuring
+
+const user = {
+    id: 123,
+    name: {
+        firstName: "shahariat",
+        middleName: "hossen",
+        lastName: "shuvo"
+    },
+    gender: "male",
+    favouriteColor: "black"
+}
+
+const { name: { firstName, middleName, lastName: myLastName }, gender: myGender, favouriteColor } = user;
+
+console.log(firstName, middleName, myLastName, myGender, favouriteColor);
+
+// array destructuring
+
+const friends = ["kata", "bata", "mata"];
+
+const [, , myBestFriend] = friends;
+console.log(myBestFriend);
+
