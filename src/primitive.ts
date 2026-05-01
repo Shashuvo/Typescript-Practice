fdaf89bdba421391c29263fd7ca5e804b7aafd52
Shashuvo <shahariat2810@gmail.com>
2026-05-01 18:01:27 +0600

    Primitive data types for TS practiced

---
+++ src/primitive.ts
@@ -0,0 +1,23 @@
+// Primitive types
+// string, boolean, number, undefined, null
+// TS: unknown, never, void
+
+
+// Explicit
+let userName: string = "Shahariat";
+
+// Implicit
+let userID = 123;
+
+// any
+let x = undefined;
+x = 5;
+x = "me";
+
+// undefined
+let y: undefined = undefined;
+y = undefined;
+
+
+// console
+console.log(userName, userID, x, y);
