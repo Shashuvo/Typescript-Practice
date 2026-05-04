bdc5fff976fb9cf039d2eab5535955ef926492f6
Shashuvo <shahariat2810@gmail.com>
2026-05-04 20:40:16 +0600

    key of constraint practiced

--- src/keyOfConstraint.ts
+++ src/keyOfConstraint.ts
@@ -1,4 +1,5 @@
-type Vehicle ={
+//keyOf operator
+type Vehicle = {
     bike: string;
     cng: string;
     car: string
@@ -6,4 +7,69 @@ type Vehicle ={
 
 type MyVehicle = keyof Vehicle;
 
-const myvehicle : MyVehicle = "bike";
+const myvehicle: MyVehicle = "bike";
+
+//key of constraint
+
+const getPropertyFromObj = <T>(obj: T, key: keyof T) => {
+    return obj[key];
+}
+
+type User = {
+    id: number;
+    name: string;
+    address: {
+        city: string;
+    };
+}
+
+
+type Product = {
+    pid: number;
+    pname: string;
+    batch: {
+        batchName: string;
+    }
+}
+
+type Student = {
+    sid: number;
+    name: string;
+    batch: {
+        year: string;
+    }
+}
+
+const user: User = {
+    id: 1,
+    name: "loli",
+    address: {
+        city: "dhaka",
+    },
+}
+
+const product: Product = {
+    pid: 2,
+    pname: "boli",
+    batch: {
+        batchName: "yellow",
+    },
+}
+
+const student: Student = {
+    sid: 3,
+    name: "koli",
+    batch: {
+        year: "2016",
+    },
+}
+
+
+
+const result1 = getPropertyFromObj(user, "id");
+
+const result2 = getPropertyFromObj(product, "pname");
+
+const result3 = getPropertyFromObj(student, "batch");
+
+console.log(result1, result2, result3);
