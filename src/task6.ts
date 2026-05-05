203f136cce2897a29ae54814564be87412afb02f
Shashuvo <shahariat2810@gmail.com>
2026-05-05 13:02:43 +0600

    keyof, generics practice task done

---
+++ src/task6.ts
@@ -0,0 +1,26 @@
+// Task 6: The Property Guard HARD
+// Concepts: keyof, Generics
+// Scenario: Create a utility that gets a property from an object while preventing typos
+// at compile-time.
+
+// const product = { id: 101, name: "Keyboard", price: 50 };
+
+// Instructions:
+// Create a function getProductProp<T, K>(obj: T, key: K).
+// Constraint K to be a valid key of T.
+// Return obj[key].
+// Hint: Use <T, K extends keyof T>.
+
+const product = {
+    id: 101,
+    name: "Keyboard",
+    price: 50
+};
+
+const getProduct = <T, K extends keyof T>(obj: T, key: K) => {
+    return obj[key];
+}
+
+const result = getProduct(product, "id");
+
+console.log(result);
